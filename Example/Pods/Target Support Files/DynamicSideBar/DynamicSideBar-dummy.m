b1a275f13fb1b331182e4c0f02c5814052fa6ea0
gomminjae <gommj0611@naver.com>
2023-01-21 18:57:42 +0900

    Feat: Foldering

---
+++ Example/Pods/Target Support Files/DynamicSideBar/DynamicSideBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DynamicSideBar : NSObject
+@end
+@implementation PodsDummy_DynamicSideBar
+@end
---
+++ Example/Pods/Target Support Files/Pods-DynamicSideBar_Tests/Pods-DynamicSideBar_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DynamicSideBar_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DynamicSideBar_Tests
+@end
